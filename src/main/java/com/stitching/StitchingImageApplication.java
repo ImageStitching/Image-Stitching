ecd87285ab9fb4b0dbdea3d2c0d2e322a352a659
CongGithub2k4 <quangdungdao28@gmail.com>
2025-10-09 10:19:13 +0700

    commit

---
+++ src/main/java/com/stitching/StitchingImageApplication.java
@@ -0,0 +1,13 @@
+package com.stitching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StitchingImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StitchingImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stitching/StitchingImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stitching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StitchingImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
